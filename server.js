ebc95a1d3240afb23e0aebaba5b783bf98433e59
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2024-10-18 03:39:54 +0500

    first

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+
 const express = require('express');
+
 const cors = require('cors');
 const connectDB = require('./config/databaseConnection');
 require('dotenv').config();
 
 const app = express();
 
-connectDB();
+
+connectDB()
+
 
 app.use(express.json());
 app.use(cors());
+
+const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api', require('./routes/customerRoutes'));
@@ -16,6 +22,6 @@ app.use('/api', require('./routes/customerRoutes'));
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to Khata API");
 });
-
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
