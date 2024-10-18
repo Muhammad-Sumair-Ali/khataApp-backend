1dd2096fb6639b03d8840e12f53a15ead5ab4124
Muhammad-Sumair-Ali <sumairjattoi@gmail.com>
2024-10-18 21:06:45 +0500

    cors add

---
+++ config/cors/origins.js
@@ -0,0 +1,3 @@
+const allowedOrigins = ["http://localhost:3000", "http://localhost:5173", "http://localhost:5174"];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-
 const express = require('express');
-
 const cors = require('cors');
 const connectDB = require('./config/databaseConnection');
 require('dotenv').config();
 
 const app = express();
 
+connectDB();
 
-connectDB()
 
+
+// Apply CORS with options
+app.use(cors({
+  origin: "http://localhost:5173"
+},));
 
 app.use(express.json());
-app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
+// Define routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api', require('./routes/customerRoutes'));
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome to Khata API");
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
